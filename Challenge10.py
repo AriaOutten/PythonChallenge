c5a1975d9c4e3c881c2fbda544e52830d69e908d
Aria Outten <aria.outten@gmail.com>
2018-02-28 19:46:37 +0100

    Cleaned up 10

--- Challenge10.py
+++ Challenge10.py
@@ -2,6 +2,7 @@
 Python Challenge 10
 Aria Outten 20190226
 '''
+
 a = ['1']
 
 for no in range(0,31):
@@ -9,26 +10,15 @@ for no in range(0,31):
 	new = ''
 	count = 1
 	for i in range(0,len(temp)):
-		if i != 0:
-			try:
-				if temp[i] == temp[i+1]:
-					count += 1
-				else:
-					new += (str(count) + temp[i])
-					count = 1
-			except Exception:
+		try:
+			if temp[i] == temp[i+1]:
+				count += 1
+			else:
 				new += (str(count) + temp[i])
 				count = 1
-		else:
-			try:
-				if temp[i] == temp[i+1]:
-					count += 1
-				else:
-					new += (str(count) + temp[i])
-					count = 1
-			except Exception:
-				new += (str(count) + temp[i])
-				count = 1
+		except Exception:
+			new += (str(count) + temp[i])
+			count = 1
 	a.append(new)
 #print(a)
 print(len(a[30]))
